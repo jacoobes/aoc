13c06eb26cadc15334be6ffc584ebe5059236a7c
Jacob Nguyen <jacob@nolaedu.com>
2025-12-04 19:43:11 -0600

    refc

--- src/y2025/d4.clj
+++ src/y2025/d4.clj
@@ -3,25 +3,12 @@
 
 ;; PROBLEM LINK https://adventofcode.com/2025/day/4
 
-;; Generator Logic
-
-;; Solution Logic
-
-;; Entry Points
-
 (defn generator
   "The generator fn is used to parse your input into. The output of this fn will be passed into each of the solving fns"
   [input]
     (->>  (clojure.string/split-lines input) (map vec) vec))
 
-(def offset [ [-1 0]
-              [-1 -1]  
-              [-1 1]
-              [1 0]
-              [1 -1]
-              [1 1]
-              [0 1]
-              [0 -1]])
+(def offset [ [-1 0] [-1 -1]  [-1 1] [1 0] [1 -1] [1 1] [0 1] [0 -1]])
 
 (defn get-removable [input]
    (->>  (for [[i row]  (vec  (map-indexed vector input))
@@ -32,13 +19,10 @@
          (map (fn [mp] (assoc mp :adj (filter  #(= % \@) (:adj mp) )) ))
          (filter (fn [mp] (< (count (:adj mp))  4)))))
 
-
 (defn solve-part-1
   "The solution to part 1. Will be called with the result of the generator"
   [input]
-    (->> ;can be refactored to use get-removable 
-        (get-removable input)
-        (count)))
+    (count (get-removable input)))
 
 (defn mark-remove [acc cur]
    (assoc-in acc (:pos cur) \.))
@@ -72,8 +56,6 @@
 
 #_(get-removable (generator sample-data))
 
-;; Tests
-;; Use tests to verify your solution. Consider using the sample data provided in the question
 
 (deftest sample-test
   (t/is (= 2 (+ 1 1))))
